c0eb55c48da1b9275e01bae4e9609f973a621e49
SearchingSteve <155838264+SearchingSteve@users.noreply.github.com>
2025-04-10 17:41:41 -0230

    feat: Create BSTData Model
    BSTData JPA model created with BST data structure and basic getters and setters

--- src/main/java/edu/keyin/stephencrocker/model/BSTData.java
+++ src/main/java/edu/keyin/stephencrocker/model/BSTData.java
@@ -1,4 +1,43 @@
 package edu.keyin.stephencrocker.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class BSTData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String inputNumbers;
+
+    @Column(columnDefinition = "TEXT")
+    private String treeJson;
+
+    public BSTData() {}
+
+    public BSTData(String inputNumbers, String treeJson) {
+        this.inputNumbers = inputNumbers;
+        this.treeJson = treeJson;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getInputNumbers() {
+        return inputNumbers;
+    }
+
+    public void setInputNumbers(String inputNumbers) {
+        this.inputNumbers = inputNumbers;
+    }
+
+    public String getTreeJson() {
+        return treeJson;
+    }
+
+    public void setTreeJson(String treeJson) {
+        this.treeJson = treeJson;
+    }
 }
