020e1c6f165832279a24f8a97ac1afad4c639e00
SearchingSteve <155838264+SearchingSteve@users.noreply.github.com>
2025-04-10 19:01:59 -0230

    feat: Create basic BSTController
    BST controller Get routing to ("/enter-numbers") endpoint to display input page

--- src/main/java/edu/keyin/stephencrocker/controller/BSTController.java
+++ src/main/java/edu/keyin/stephencrocker/controller/BSTController.java
@@ -1,4 +1,22 @@
 package edu.keyin.stephencrocker.controller;
 
+import edu.keyin.stephencrocker.service.BSTService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@Controller
 public class BSTController {
+
+    @Autowired
+    private BSTService bstService;
+
+    @GetMapping("/enter-numbers")
+    public String showInputPage() {
+        return "input-page";
+    }
+
+
 }
