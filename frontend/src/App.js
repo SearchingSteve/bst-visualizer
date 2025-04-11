63195058e359d62479902093004902398c2afcc5
SearchingSteve <155838264+SearchingSteve@users.noreply.github.com>
2025-04-11 17:49:59 -0230

    feat: Create React App

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
